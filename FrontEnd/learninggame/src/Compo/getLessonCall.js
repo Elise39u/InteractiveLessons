7b1015e756e7547af97bb12fb570644b1b307cc2
Elise <justin555@live.nl>
2022-11-16 15:20:59 +0100

    Styling fixed the way i imagined it

--- FrontEnd/learninggame/src/Compo/getLessonCall.js
+++ FrontEnd/learninggame/src/Compo/getLessonCall.js
@@ -43,12 +43,12 @@ class GetLessonCall extends React.Component {
                               if(subjectId !== lesson.lessonSubject.subjectId) {
                                   subjectId = lesson.lessonSubject.subjectId;
                                   return [ <h1> {lesson.lessonSubject.subjectName} </h1>,
-                                      <Col> <Button variant={"warning"}> {lesson.lessonName} </Button> </Col> ]
+                                      <Col md={3}> <Button variant={"warning"}> {lesson.lessonName} </Button> </Col>  ]
                               } else {
                                   if (lesson.isLocked === true) {
-                                      return  <Col>   <Button variant={"danger"} disabled> {lesson.lessonName} </Button> </Col>
+                                      return  <Col md={3}>   <Button variant={"danger"} disabled> {lesson.lessonName} </Button> </Col>
                                   } else {
-                                      return <Col>   <Button variant={"warning"}> {lesson.lessonName} </Button> </Col>
+                                      return <Col md={3}>   <Button variant={"warning"}> {lesson.lessonName} </Button> </Col>
                                   }
                               }
                           })
